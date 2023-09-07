2a1dca7bc02fcd487feb5f577946f1015adb39b1
calware <47096877+calware@users.noreply.github.com>
2023-09-06 20:29:52 -0700

    Just the static files

---
+++ _next/static/chunks/app/college-admissions-and-applications/key-takeaways/get-involved/page-cd11efcdc9fecca2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[619,160,580,861,547,510,696,434,755,280,866],{744:function(n,e,u){Promise.resolve().then(u.t.bind(u,3222,23))}},function(n){n.O(0,[222,971,596,744],function(){return n(n.s=744)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/college-admissions-and-applications/key-takeaways/summary/page-780ab772795670c9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[642,931,835,612],{3521:function(){}},function(n){n.O(0,[971,596,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
